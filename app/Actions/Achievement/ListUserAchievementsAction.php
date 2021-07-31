407708f1f46a5e2167fa9605abf5c5cb61696790
Amr Elsayed <amr-elsayed@outlook.com>
2021-07-31 14:06:34 +0200

    refactor list user achievements controller logic to actions

---
+++ app/Actions/Achievement/ListUserAchievementsAction.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Actions\Achievement;
+
+use App\Models\Achievement;
+use App\Models\Badge;
+use App\Models\User;
+
+class ListUserAchievementsAction
+{
+	public function execute(User $user)
+	{
+		$unlocked_achievements = $user->achievements()->pluck('name')->toArray();
+
+		$next_available_achievements = [];
+
+		$next_lesson_watched_achievement = $this->nextAvailableAchievement($user, Achievement::TYPES['LESSON_WATCHED']);
+        
+        if ($next_lesson_watched_achievement) {
+            $next_available_achievements[] = $next_lesson_watched_achievement->name;
+        }
+
+        $next_comment_written_achievement = $this->nextAvailableAchievement($user, Achievement::TYPES['COMMENT_WRITTEN']);
+
+        if ($next_comment_written_achievement) {
+            $next_available_achievements[] = $next_comment_written_achievement->name;
+        }
+
+        $next_badge = Badge::where('points', '>', $user->badge->points)
+            ->first();
+
+        $remaing_to_unlock_next_badge = 0;
+        
+        if ($next_badge) {
+            $remaing_to_unlock_next_badge = $next_badge->points - count($unlocked_achievements);
+        }
+
+        return [
+            'unlocked_achievements' => $unlocked_achievements,
+            'next_available_achievements' => $next_available_achievements,
+            'current_badge' => $user->badge->name,
+            'next_badge' => $next_badge ? $next_badge->name : '',
+            'remaing_to_unlock_next_badge' => $remaing_to_unlock_next_badge
+        ];
+	}
+
+	private function nextAvailableAchievement(User $user, int $type)
+	{
+		$latest_reserved_achievement = $user->achievements()
+            ->where('type', $type)
+            ->latest('id')
+            ->first();
+        
+        return $next_available_achievement = Achievement::where('type', $type)
+            ->where('points', '>', $latest_reserved_achievement->points)
+            ->first();
+	}
+}
--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -2,60 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Achievement;
-use App\Models\Badge;
+use App\Actions\Achievement\ListUserAchievementsAction;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
-    public function index(User $user)
-    {
-        $unlocked_achievements = $user->achievements()->pluck('name')->toArray();
+    public function index(
+        User $user, 
+        ListUserAchievementsAction $listUserAchievementsAction
+    ) {
+        $user_achievments = $listUserAchievementsAction->execute($user);
 
-        $latest_lesson_watched_achievement = $user->achievements()
-            ->where('type', Achievement::TYPES['LESSON_WATCHED'])
-            ->latest('id')
-            ->first();
-        
-        $next_lesson_watched_achievement = Achievement::where('type', Achievement::TYPES['LESSON_WATCHED'])
-            ->where('points', '>', $latest_lesson_watched_achievement->points)
-            ->first();
-        
-        $latest_comment_written_achievement = $user->achievements()
-            ->where('type', Achievement::TYPES['COMMENT_WRITTEN'])
-            ->latest('id')
-            ->first();
-
-        $next_comment_written_achievement = Achievement::where('type', Achievement::TYPES['COMMENT_WRITTEN'])
-            ->where('points', '>', $latest_comment_written_achievement->points)
-            ->first();
-
-        $next_available_achievements = [];
-
-        if ($next_lesson_watched_achievement) {
-            $next_available_achievements[] = $next_lesson_watched_achievement->name;
-        }
-
-        if ($next_comment_written_achievement) {
-            $next_available_achievements[] = $next_comment_written_achievement->name;
-        }
-
-        $next_badge = Badge::where('points', '>', $user->badge->points)
-            ->first();
-
-        $remaing_to_unlock_next_badge = 0;
-        
-        if ($next_badge) {
-            $remaing_to_unlock_next_badge = $next_badge->points - count($unlocked_achievements);
-        }
-
-        return response()->json([
-            'unlocked_achievements' => $unlocked_achievements,
-            'next_available_achievements' => $next_available_achievements,
-            'current_badge' => $user->badge->name,
-            'next_badge' => $next_badge ? $next_badge->name : '',
-            'remaing_to_unlock_next_badge' => $remaing_to_unlock_next_badge
-        ]);
+        return response()->json($user_achievments);
     }
 }
